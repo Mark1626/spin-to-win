a26b8f1ac36b4e7180f3b33eb46676216dbb598c
M4rk9696 <nimalan.m@protonmail.com>
2020-03-22 22:32:49 +0530

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ManifestPlugin = require("webpack-manifest-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const InterpolateHtmlPlugin = require("react-dev-utils/InterpolateHtmlPlugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const safePostCssParser = require("postcss-safe-parser");
 const fs = require("fs");
 const url = require("url");
 const path = require("path");
@@ -15,8 +22,34 @@ function ensureSlash(inputPath, needsSlash) {
   }
 }
 
+const moduleFileExtensions = [
+  "web.mjs",
+  "mjs",
+  "web.js",
+  "js",
+  "web.ts",
+  "ts",
+  "web.tsx",
+  "tsx",
+  "json",
+  "web.jsx",
+  "jsx"
+];
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+const resolveModule = (resolveFn, filePath) => {
+  const extension = moduleFileExtensions.find(extension =>
+    fs.existsSync(resolveFn(`${filePath}.${extension}`))
+  );
+
+  if (extension) {
+    return resolveFn(`${filePath}.${extension}`);
+  }
+
+  return resolveFn(`${filePath}.js`);
+};
+const appIndexJs = resolveModule(resolveApp, "src/index");
+const appBuild = resolveApp("build");
 const getPublicUrl = appPackageJson => require(appPackageJson).homepage;
 function getServedPath(appPackageJson) {
   const publicUrl = getPublicUrl(appPackageJson);
@@ -24,17 +57,75 @@ function getServedPath(appPackageJson) {
   return ensureSlash(servedUrl, true);
 }
 const publicPath = getServedPath(resolveApp("package.json"));
+const publicUrl = publicPath.slice(0, -1);
 const imageInlineSizeLimit = parseInt(
   process.env.IMAGE_INLINE_SIZE_LIMIT || "10000"
 );
+const env = {
+  ...process.env,
+  PUBLIC_URL: publicUrl
+};
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "production",
+  entry: appIndexJs,
   output: {
-    path: __dirname + "/dist",
-    filename: "bundle.js"
+    path: appBuild,
+    pathinfo: false,
+    filename: "static/js/[name].[contenthash:8].js",
+    futureEmitAssets: true,
+    chunkFilename: "static/js/[name].[contenthash:8].chunk.js",
+    publicPath: publicPath
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // This is only used in production mode
+      new TerserPlugin({
+        terserOptions: {
+          parse: {
+            ecma: 8
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            comparisons: false,
+            inline: 2
+          },
+          mangle: {
+            safari10: true
+          },
+          keep_classnames: false,
+          keep_fnames: false,
+          output: {
+            ecma: 5,
+            comments: false,
+            ascii_only: true
+          }
+        },
+        parallel: true,
+        cache: true,
+        sourceMap: false
+      }),
+      // This is only used in production mode
+      new OptimizeCSSAssetsPlugin({
+        cssProcessorOptions: {
+          parser: safePostCssParser,
+          map: false
+        }
+      })
+    ],
+    splitChunks: {
+      chunks: "all",
+      name: false
+    },
+    runtimeChunk: {
+      name: entrypoint => `runtime-${entrypoint.name}`
+    }
   },
   module: {
+    strictExportPresence: true,
+
     rules: [
       {
         test: /\.js$/,
@@ -58,10 +149,22 @@ module.exports = {
             name: "static/media/[name].[hash:8].[ext]"
           }
         }
+      },
+      {
+        loader: require.resolve("file-loader"),
+        // Exclude `js` files to keep "css" loader working as it injects
+        // its runtime that would otherwise be processed through "file" loader.
+        // Also exclude `html` and `json` extensions so they get processed
+        // by webpacks internal loaders.
+        exclude: [/\.(js|mjs|jsx|ts|tsx|css)$/, /\.html$/, /\.json$/],
+        options: {
+          name: "static/media/[name].[hash:8].[ext]"
+        }
       }
     ]
   },
   plugins: [
+    new CopyWebpackPlugin([{ from: "public" }]),
     new HtmlWebPackPlugin(
       Object.assign(
         {},
@@ -85,6 +188,7 @@ module.exports = {
         }
       )
     ),
+    new InterpolateHtmlPlugin(HtmlWebPackPlugin, env),
     new ManifestPlugin({
       fileName: "asset-manifest.json",
       publicPath: publicPath,
@@ -102,6 +206,21 @@ module.exports = {
           entrypoints: entrypointFiles
         };
       }
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      clientsClaim: true,
+      exclude: [/\.map$/, /asset-manifest\.json$/],
+      importWorkboxFrom: "cdn",
+      navigateFallback: publicUrl + "/index.html",
+      navigateFallbackBlacklist: [
+        // Exclude URLs starting with /_, as they're likely an API call
+        new RegExp("^/_"),
+        // Exclude any URLs whose last part seems to be a file extension
+        // as they're likely a resource and not a SPA route.
+        // URLs containing a "?" character won't be blacklisted as they're likely
+        // a route with query params (e.g. auth callbacks).
+        new RegExp("/[^/?]+\\.[^/]+$")
+      ]
     })
   ]
 };
